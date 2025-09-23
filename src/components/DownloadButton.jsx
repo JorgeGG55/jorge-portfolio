0537ae275ab48a97071f4c441f636b90a19af671
Jorge Gravel <jorge.gravel.g@gmail.com>
2025-09-23 13:14:46 +0200

    Cambios descargas cv y actualización

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,20 +1,21 @@
-// src/components/DownloadButton.jsx
+// src/components/DownloadButton.jsx 
 import React from "react";
 import { useTranslation } from "react-i18next";
 
 const DownloadButton = () => {
-  const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    const currentLanguage = i18n.language;
 
-  return (
-    <a
-      href="/CV-Jorge-Gravel.pdf"
-      download
-      className="uppercase flex items-center gap-2 border-solid border border-secondary text-secondary py-3 px-8 rounded-full tracking-widest transition-all duration-500 hover:bg-secondary hover:text-dark hover:font-bold"
-    >
-      <span>{t("downloadCV")}</span>
-      <i className="fas fa-download"></i>
-    </a>
-  );
+    const cvFile = currentLanguage === "es" ? "/CV-Jorge-Gravel (ES).pdf" : "/CV-Jorge-Gravel-(EN).pdf";
+
+    return (
+        <a
+            href={cvFile}
+            download className="uppercase flex items-center gap-2 border-solid border border-secondary text-secondary py-3 px-8 rounded-full tracking-widest transition-all duration-500 hover:bg-secondary hover:text-dark hover:font-bold" >
+            <span>{t("downloadCV")}</span>
+            <i className="fas fa-download"></i>
+        </a>
+    );
 };
 
 export default DownloadButton;
