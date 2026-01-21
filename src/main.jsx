779a26df6f1f2606a316f2ce10aab36a9e2aec9e
Jorge Gravel <jorge.gravel.g@gmail.com>
2026-01-21 18:19:24 +0100

    Add GA4 page tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
+import AnalyticsTracker from "./components/AnalyticsTracker";
 import App from "./App.jsx";
 import "../i18n.js";
 import "./index.css";
@@ -10,10 +12,15 @@ const Resume = lazy(() => import("./components/Resume"));
 const Projects = lazy(() => import("./components/Projects"));
 const Contact = lazy(() => import("./components/Contact"));
 
+ReactGA.initialize("G-2B1Q0STST8");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <AnalyticsTracker />
+
       <App />
+
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
